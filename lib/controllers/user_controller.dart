5ed4685524822f59aeaf7712d71cb3dc6f657435
mbarahon <manuel.barahona@usm.cl>
2024-10-13 21:43:43 -0300

    user model

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -18,4 +18,19 @@ class UserController {
     await _db.collection("users").doc(_auth.currentUser?.uid).set(user.toFirestore());
   }
 
+  Future<void> loginWithUsername(String username, String password) async{
+
+    final userSnapshot = await _db.collection("users").where("Nombre", isEqualTo: username).get();
+    DocumentSnapshot userDoc = userSnapshot.docs.first;
+
+    await _auth.signInWithEmailAndPassword(
+      email: userDoc["Correo"], 
+      password: password
+    );
+
+    final userId = _auth.currentUser?.email;
+
+    print("el usuario actual es $userId");
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tcgarchive/controllers/cards_controller.dart';
+import 'package:tcgarchive/controllers/folders_controller.dart';
 import 'package:tcgarchive/firebase_options.dart';
 import 'package:tcgarchive/controllers/user_controller.dart';
 import 'package:tcgarchive/models/user_model.dart';
@@ -23,14 +24,24 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     final UserController _userController = UserController();
+    final FoldersController _foldersController = FoldersController();
     final UserModel user = UserModel(
       usernName: "mrmerluso",
       email: "mrmerluso@hotmail.com",
     );
     // _userController.createUserInFirestore(user, "password");
 
+    _userController.loginWithUsername("mrmerluso", "password");
+
+    // _foldersController.createFolder("Carpeta1", "pkmn");
+
+    List<String> cards = List.from(["JBWaheB0DjCLCKqLiRhZ", "SVqKzyFkJNMMjtPAeEkG"]);
+    // _foldersController.addCardsToFolder(cards, "iRsZN1ZERBP4VJyUZMN5");
+    // _foldersController.addCardToFolder("JBWaheB0DjCLCKqLiRhZ", "iRsZN1ZERBP4VJyUZMN5", 4);
+    _foldersController.getCardsFromFolder("iRsZN1ZERBP4VJyUZMN5");
+
     final CardsController _cardsController = CardsController();
-    _cardsController.getPkmCards();
+    // _cardsController.getPkmCards();
 
     return const MaterialApp(
       home: Scaffold(
